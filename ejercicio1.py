7e7b5d32482634e2ce63ae7283b9a1bc9d0093f3
AndreRivaldo <rivaldo.andre@gmail.com>
2026-07-07 18:26:21 -0400

    Agrega ejercicio 1 y 2

---
+++ ejercicio1.py
@@ -0,0 +1,3 @@
+resultado = 2 * 3 + 4 - 12 ** 5
+
+print("El resultado de 2*3+4-12**5 es:", resultado)
---
+++ ejercicio2.py
@@ -0,0 +1,7 @@
+a = float(input("ingresa el primer número: "))
+
+b = float(input("ingresa el segundo número: "))
+
+area = a * b
+
+print("El área del rectángulo es:", area)
