b73fbf8367cb3a864b0686f1399511b758b18b5d
AndreRivaldo <rivaldo.andre@gmail.com>
2026-07-08 17:42:59 -0400

    Agrega ejercicio 3

---
+++ ejercicio3.py
@@ -0,0 +1,16 @@
+respuesta = input ("¿Te gustan los dulces? reesponda la pregunta con un si o un no:")
+
+if respuesta == "si":
+    print ("¡A mi tambien!")
+
+elif respuesta == "SI":
+    print ("¡A mi tambien!")
+
+elif respuesta == "Sí":
+    print ("¡A mi tambien!")
+
+elif respuesta == "sí":
+    print ("¡A mi tambien!")
+
+else:
+    print ("¡Gracias por tu respuesta!")
