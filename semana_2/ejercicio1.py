c478a78b8fbac1cdc339fbee6b7d764c91b8d835
AndreRivaldo <rivaldo.andre@gmail.com>
2026-07-14 17:43:17 -0400

    Agregar ejecici1 semana 2

---
+++ semana_2/ejercicio1.py
@@ -0,0 +1,11 @@
+# Llene una lista con números enteros y calcule la suma de ellos.
+# La lista debe ser creada manualmente con distintos números y cantidad de elementos.
+
+numeros = [8, -3, 15, 4, 11, 2]
+suma = 0
+
+for numero in numeros:
+    suma += numero
+
+print("Lista de números:", numeros)
+print("La suma de los números es:", suma)
